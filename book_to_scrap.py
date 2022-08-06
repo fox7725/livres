1f95d9bfc8f11249e50ad9f8835e5b32b9cc5ef1
fox77 <fox7725@gmail.com>
2022-08-06 11:10:32 +0200

    Modification du nom de certaines variables pour éviter les conflits

--- book_to_scrap.py
+++ book_to_scrap.py
@@ -4,9 +4,9 @@ import requests
 from bs4 import BeautifulSoup
 
 #On charge la page de l'article en HTML
-url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
+url_livre = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
+page_livre = requests.get(url_livre)
+soup_livre = BeautifulSoup(page_livre.content, "html.parser")
 
 #On place la future entête du CSV dans une liste
 entete = ["product_page_url", "universal_product_code (UPC)", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "category", "review_rating", "image_url"]
@@ -20,10 +20,10 @@ entete = ["product_page_url", "universal_product_code (UPC)", "title", "price_in
     #récupération de l'url de la page
 product_page_url = url
     #récupération du titre du livre (seul élément en <h1>
-title = soup.h1.string
+title = soup_livre.h1.string
     #Pour récupérer les éléments en bas de page qui sont sous forme de
     #tableau, on créé deux listes avec les <th> et les <td> et on fait un dictionnaire
-tableau = soup.find("table", class_="table-striped")
+tableau = soup_livre.find("table", class_="table-striped")
 nom = tableau.find_all("th")
 valeur = tableau.find_all("td")
 liste_noms = []
@@ -40,22 +40,22 @@ price_excluding_tax = dictionnaire_tableau["Price (excl. tax)"]
 number_available = dictionnaire_tableau["Availability"]
     #pour la description, on récupère le texte enfant de la classe product_page
     #on trasnforme la liste obtenue en variable simple et on récupère le txt
-product_description = soup.select(".product_page > p")
+product_description = soup_livre.select(".product_page > p")
 product_description = product_description[0].text
     #Pour récupérer la catégorie qui se trouve dans le breadcrumbs qu'on va
     #transformer en liste et dont on va récupérer l'avant dernière valeur
     #on ne récupère que le texte
-category = soup.select(".breadcrumb > li")
+category = soup_livre.select(".breadcrumb > li")
 category = category[-2].find("a").string
     #pour l'url de l'image, on va dans la classe "active" pour récupérer
     #l'attribut "src" et il faut remplacer "../.." par la racine du site
-image_url = soup.select(".active > img")
+image_url = soup_livre.select(".active > img")
 image_url = image_url[0]
 image_url = image_url.get("src")
 image_url = image_url.replace("../..", "https://books.toscrape.com")
     #Pour récupérer le nombre d'étoiles, on peut voir qu'il est clairement
     #mis dans la classe, qu'il faut donc récupérer
-review_rating = soup.find("p", {"class": "star-rating"}).get("class")
+review_rating = soup_livre.find("p", {"class": "star-rating"}).get("class")
 review_rating = review_rating[1]
 if review_rating == "One":
     review_rating="1 star"
--- liens_pages.py
+++ liens_pages.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 # On chage la page en HTML
 category_url = "https://books.toscrape.com/catalogue/category/books/add-a-comment_18/"
-url = category_url + "index.html"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-liste_livres = soup.select(".image_container > a")
+url_page = category_url + "index.html"
+page_page = requests.get(url_page)
+soup_page = BeautifulSoup(page_page.content, 'html.parser')
+liste_livres = soup_page.select(".image_container > a")
 liste_livres_url = []
 for livre in liste_livres:
     livre_clean = livre.get("href")
@@ -13,17 +13,17 @@ for livre in liste_livres:
     liste_livres_url.append(livre_url)
 
 #On vérifie s'i y a un bouton "next"
-next_page = soup.find("li", class_="next")
+next_page = soup_page.find("li", class_="next")
 #On créé une boucle pour les pages suivante
 while next_page != None:
     next_page_url = category_url + next_page.find("a").get("href")
-    page = requests.get(next_page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    liste_livres = soup.select(".image_container > a")
+    page_page = requests.get(next_page_url)
+    soup_page = BeautifulSoup(page_page.content, 'html.parser')
+    liste_livres = soup_page.select(".image_container > a")
     for livre in liste_livres:
         livre_clean = livre.get("href")
         livre_url = livre_clean.replace("../../..", "https://books.toscrape.com/catalogue")
         liste_livres_url.append(livre_url)
-    next_page = soup.find("li", class_="next")
+    next_page = soup_page.find("li", class_="next")
 print(liste_livres_url)
 print(len(liste_livres_url))
